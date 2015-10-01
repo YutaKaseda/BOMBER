564f160297f8fb20390b2be977de92071d24271d
Keigo Nakamura <k5nakamura0120@yahoo.co.jp>
2015-10-01 18:33:43 +0900

    2015/10/01 ボムの設置 クリック移動

--- Assets/bomb.cs
+++ Assets/bomb.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class bomb : MonoBehaviour {
 
+	Vector2 fastMousePosition;
+	Vector2 nowMousePosition;
+	Vector2 lastMousePosition;
+
+
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +17,28 @@ public class bomb : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			fastMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Debug.Log ("FMP "+fastMousePosition);
+		}
+
+		if (Input.GetMouseButton (0)) {
+			nowMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			transform.position = nowMousePosition;
+			Debug.Log ("NMP "+nowMousePosition);
+		}
+		if(Input.GetMouseButtonUp(0)){
+
+			lastMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);		
+			Debug.Log ("LMP "+lastMousePosition);
+
+		}
 	
 	}
+
+
+
+
 }
