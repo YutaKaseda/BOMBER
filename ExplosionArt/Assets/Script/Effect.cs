a4395c4ec974951a87c6c7dea5f8c97ec8802dd6
yukimiyamoto <yuukimiyamoto0520@yahoo.co.jp>
2015-10-05 10:33:08 +0900

    画面比率の調整とパーティクルの設定

---
+++ ExplosionArt/Assets/Script/Effect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Effect : MonoBehaviour
+{
+    ParticleSystem effect;
+
+	// Use this for initialization
+	void Start ()
+    {
+        effect = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("爆発のエフェクト消滅");
+        if (effect.isPlaying == false) Destroy(gameObject);
+	}
+}
