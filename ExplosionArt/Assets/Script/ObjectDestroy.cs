c2bf9a81b58e7fe1d314c6e2a1008a7b9850f53c
yukimiyamoto <yuukimiyamoto0520@yahoo.co.jp>
2015-10-05 11:46:05 +0900

    ぶつかった時のエフェクトを追加

---
+++ ExplosionArt/Assets/Script/ObjectDestroy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDestroy : MonoBehaviour
+{
+    public GameObject EffectPrefab;
+    public string Tag = "";
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.tag == Tag)
+        {
+            Destroy(other.gameObject);
+
+            if (EffectPrefab != null)
+            {
+                Debug.Log("爆発エフェクト作るよ");
+                Instantiate(
+                    EffectPrefab,
+                    other.transform.position,
+                    Quaternion.identity
+                    );
+            }
+        }
+    }
+}
