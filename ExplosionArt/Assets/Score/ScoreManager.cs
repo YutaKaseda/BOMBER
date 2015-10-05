3a0fa57c75a3b41bcd80bb5cdfdbcf73437624a5
kaseda <hieharu3216@gmail.com>
2015-10-05 17:27:40 +0900

    元に戻しました（怒）

---
+++ ExplosionArt/Assets/Score/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+    public float score { get; private set; }
+    private float scoreYard;
+
+	// Use this for initialization
+	private void Awake () {
+        score = 0.0f;
+        scoreYard = 0.0f;
+	}
+
+    private void Update(){
+
+        if (scoreYard > 0)
+        {
+            score += 100;
+            scoreYard -= 100;
+        }
+
+    }
+
+    public void plusScore(float plus){
+        Debug.Log(plus + "分スコア加算");
+        scoreYard += plus;
+
+    }
+
+}
